3e0cb95693b02872fa287ef06fe384949f62a9e7
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-06-23 11:45:13 +0200

    initial commit

---
+++ egdb_custom_text/custom_toolbar.php
@@ -0,0 +1,5 @@
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/expression_input.php">Expression Viewer</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/comparator_input.php">Expression comparator</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/expression_menu.php">Datasets</a></li>
+<li class="nav-item"><a class="nav-link" href="https://peatmoss.plantcode.cup.uni-freiburg.de/expression_viewer/input" target="_blank">PEATmoss</a></li>
+<li class="nav-item"><a class="nav-link" href="https://peatmoss.plantcode.cup.uni-freiburg.de/ppatens_db/pp_search_input.php" target="_blank">PpGMLDB</a></li>
---
+++ egdb_custom_text/expr_datasets/pp_sex_rep.php
@@ -0,0 +1,35 @@
+<p>
+  RNA-seq of <i>P. patens</i> ... General description here.
+</p>
+<h4>Experimental Conditions (3 replicates unless otherwise noted):</h4>
+
+<ul>
+  <li><span style="color:blue;">Re_Gam_apice_noW (Reute gametophore apices without watering)</span></li>
+  <li><span style="color:blue;">Re_Gam_apice_2_DAW (Reute gametophore apices 2 days after watering)</span></li>
+  <li><span style="color:green;">Re_Antheridia_Bundles (Reute antheridia bundles)</span></li>
+  <li><span style="color:green;">Re_Antheridia_9_DAI (Reute antheridia 9 days after)</span></li>
+  <li><span style="color:green;">Re_Antheridia_11_DAI (Reute antheridia 11 days after)</span> [5 replicates]</li>
+  <li><span style="color:green;">Re_Antheridia_14-15_DAI (Reute antheridia 14-15 days after)</span> [4 replicates]</li>
+  <li><span style="color:green;">Re_Sperm_Cell_Packages (Reute sperm cell packages)</span></li>
+  <li><span style="color:green;">Re_Mature_Archegonia (Reute mature archegonia)</span></li>
+  <li><span style="color:orange;">Vx_EggCell_Mat_Archegonia (Villersexel egg cell mature archegonia)</span></li>
+  <li><span style="color:orange;">Vx_Unfertilized_Archegonia (Villersexel unfertilized archegonia)</span></li>
+  <li><span style="color:pink;">Vx_F (Villersexel F)</span></li>
+  <li><span style="color:pink;">Vx_F_1/2 (Villersexel F 1/2)</span></li>
+  <li><span style="color:pink;">Vx_ES1 (Villersexel ES1)</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx_F1/E1/E2 (Villersexel F1/E1/E2)</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx_ES/ES1 (Villersexel ES/ES1)</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx_Green_Sporophyte (Villersexel Green Sporophyte)</span> [1 replicate]</li>
+  
+  
+</ul>
+
+  
+<p>
+  Source of samples and publications here.
+</p>
+<p>
+  All samples were normalized to RPKM.
+</p>
+
+
