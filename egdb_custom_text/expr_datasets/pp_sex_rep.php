d0c7edc407e360280bfa6f95072ce02a2777d700
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-08-28 16:39:23 +0200

    description improved

--- egdb_custom_text/expr_datasets/pp_sex_rep.php
+++ egdb_custom_text/expr_datasets/pp_sex_rep.php
@@ -1,35 +1,96 @@
 <p>
-  RNA-seq of <i>P. patens</i> ... General description here.
+  RNA-seq expression data of <i>P. patens</i> experiments conducted in sexual organs and first stages of the sporophyte development. All samples were normalized to RPKM.
 </p>
 <h4>Experimental Conditions (3 replicates unless otherwise noted):</h4>
 
+
 <ul>
-  <li><span style="color:blue;">Re_Gam_apice_noW (Reute gametophore apices without watering)</span></li>
-  <li><span style="color:blue;">Re_Gam_apice_2_DAW (Reute gametophore apices 2 days after watering)</span></li>
-  <li><span style="color:green;">Re_Antheridia_Bundles (Reute antheridia bundles)</span></li>
-  <li><span style="color:green;">Re_Antheridia_9_DAI (Reute antheridia 9 days after)</span></li>
-  <li><span style="color:green;">Re_Antheridia_11_DAI (Reute antheridia 11 days after)</span> [5 replicates]</li>
-  <li><span style="color:green;">Re_Antheridia_14-15_DAI (Reute antheridia 14-15 days after)</span> [4 replicates]</li>
-  <li><span style="color:green;">Re_Sperm_Cell_Packages (Reute sperm cell packages)</span></li>
-  <li><span style="color:green;">Re_Mature_Archegonia (Reute mature archegonia)</span></li>
-  <li><span style="color:orange;">Vx_EggCell_Mat_Archegonia (Villersexel egg cell mature archegonia)</span></li>
-  <li><span style="color:orange;">Vx_Unfertilized_Archegonia (Villersexel unfertilized archegonia)</span></li>
-  <li><span style="color:pink;">Vx_F (Villersexel F)</span></li>
-  <li><span style="color:pink;">Vx_F_1/2 (Villersexel F 1/2)</span></li>
-  <li><span style="color:pink;">Vx_ES1 (Villersexel ES1)</span> [1 replicate]</li>
-  <li><span style="color:pink;">Vx_F1/E1/E2 (Villersexel F1/E1/E2)</span> [1 replicate]</li>
-  <li><span style="color:pink;">Vx_ES/ES1 (Villersexel ES/ES1)</span> [1 replicate]</li>
-  <li><span style="color:pink;">Vx_Green_Sporophyte (Villersexel Green Sporophyte)</span> [1 replicate]</li>
+  <li><span style="color:blue;">Re - Gam apice noW (gametophore apices without watering)</span></li>
+  <li><span style="color:blue;">Re - Gam apice 2 DAW (gametophore apices 2 days after watering)</span></li>
+  <li><span style="color:green;">Re - Antheridia Bundles</span></li>
+  <li><span style="color:green;">Re - Antheridia 9 DAI (antheridia 9 days after)</span></li>
+  <li><span style="color:green;">Re - Antheridia 11 DAI (antheridia 11 days after)</span> [5 replicates]</li>
+  <li><span style="color:green;">Re - Antheridia 14-15 DAI (antheridia 14-15 days after)</span> [4 replicates]</li>
+  <li><span style="color:green;">Re - Sperm Cell Packages</span></li>
+  <li><span style="color:green;">Re - Mature Archegonia</span></li>
+  <li><span style="color:orange;">Vx - EggCell Mat Archegonia (egg cell mature archegonia)</span></li>
+  <li><span style="color:orange;">Vx - Unfertilized Archegonia</span></li>
+  <li><span style="color:pink;">Vx - F</span></li>
+  <li><span style="color:pink;">Vx - F1/2</span></li>
+  <li><span style="color:pink;">Vx - ES1</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx - F1/E1/E2</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx - ES/ES1</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx - Green Sporophyte</span> [1 replicate]</li>
   
   
 </ul>
 
-  
 <p>
-  Source of samples and publications here.
-</p>
-<p>
-  All samples were normalized to RPKM.
+  Re: Reute WT<br>
+  Vx: Villersexel WT<br>
+  noW: 2 days without watering<br>
+  2 DAW: 2 days after watering<br>
+  DAI: days after <br>
+  Note: Gametophores apices were collected to study gene expression in sexual organs.
 </p>
 
 
+<center>
+<img src="/MAdLandExpr/egdb_images/expr/peatmoss_mod_arranged.png" style="width:900px">
+<p>Figure 1. Sporophyte developmental stages</p>
+</center>
+
+<table class="spodev_table">
+<tr style="border-bottom: solid 1px">
+    <td>Abb.</td>
+    <td style="width:210px"><b>Developmental stage</b></td>
+    <td><b>Description</b></td>
+  </tr>
+  <tr>
+    <td>E1</td>
+    <td><b>Embryo 1:</b><br></td>
+    <td>Developing embryo; the upper, chloroplast rich half will develop into  the spherical,
+    spore-containing spore capsule, whereas the lower part  will connect to the gametophore for 
+    nourishment of the developing  sporophyte.
+    </td>
+  </tr>
+  <tr>
+    <td>E2</td>
+    <td><b>Embryo 2:</b><br></td>
+    <td>Elongated embryo without developed stomata. Connection between
+gametophore and sporophyte still loose but cells started to differentiate.</td>
+  </tr>
+  <tr>
+    <td>ES</td>
+    <td><b>Early sporophyte:</b></td>
+    <td>Stomata are developed, clear separation of capsule  (inflation) and developing seta (2n).</td>
+  </tr>
+  <tr>
+    <td>PM</td>
+    <td><b>Premeiotic sporophyte:</b></td>
+    <td>Spherical green translucend sporophyte containing spore mother cells (2n), seta starts to turn brownish.</td>
+  </tr>
+  <tr>
+    <td>M</td>
+    <td><b>Meiotic sporophyte:</b></td>
+    <td>Opaque green sporophyte containing tetrades which will later form each four spores (1n, meiosis occured).</td>
+  </tr>
+  <tr>
+    <td>Y</td>
+    <td><b>Yellow sporophyte:</b></td>
+    <td>Contains early spores of the final size covered by a plasma membrane, seta darkend during the maturation process.</td>
+  </tr>
+  <tr>
+    <td>LB</td>
+    <td><b>Light brown sporophyte:</b></td>
+    <td>Spores started to mature, the spore wall thickens by exine deposition.</td>
+  </tr>
+  <tr style="border-bottom: solid 1px; padding-bottom:20px;height:45px">
+    <td>B</td>
+    <td><b>Brown sporophyte:</b></td>
+    <td>Spores are mature, spore wall consists of a thick perine layer with spikes, exine, seperating layer and intine, sporophyte detaches easily.</td>
+  </tr>
+</table>
+
+<br>
+The experiments of this dataset were published in <a href="https://doi.org/10.1007/s00497-021-00409-0">Genau et al., 2021</a> (Gametophore apices), <a href="https://doi.org/10.1111/nph.16486">Meyberg et al., 2020</a> (antheridia bundles), <a href="https://doi.org/10.3389/fpls.2020.00813">Haas et al., 2020</a> (Egg cell, unfertilized archegonia, F, and F1/2), <a href="https://doi.org/10.1093%2Fgbe%2Fevx098">Szövényi et al., 2020</a> (F1/E1/E2, ES/ES1, Green sporophyte), <a href="https://doi.org/10.1038/s41477-021-00958-2">Julca et al., 2021</a> (antheridia development, sperm cell packages, and mature archegonia). All samples were normalized to RPKM.
