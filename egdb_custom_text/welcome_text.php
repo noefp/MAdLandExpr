69841903f12d87a8e9d8df61d0c477548fcdde5a
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-11-29 11:05:54 +0100

    basic text and citation added

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -1,24 +1,26 @@
       <h1>Welcome to <?php echo "$dbTitle";?> </h1>
-      <h2>Subtitle h2</h2>
-      <h3>Subtitle h3</h3>
+
       <h4 class="p_font18">
-        This is a template to write your own welcome page. 
-        Please, follow the instructions to create your own custom_text folder and include the path in the configuration file.
-        
+        This site is a module of PEATmoss for gene expression visualization and comparison.
       </h4>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit mus nisi vel risus semper sagittis vitae, 
-        luctus platea quisque cum nascetur sem ligula leo magnis cursus cubilia pulvinar. 
-        Habitasse praesent platea nunc pharetra laoreet etiam congue proin, nostra vehicula nec quis iaculis molestie integer diam viverra, 
-        primis nullam gravida massa tempor curae metus. Vitae vivamus nascetur eros mus viverra nostra fames sagittis faucibus donec, 
-        cubilia interdum volutpat augue nisl libero fermentum proin quisque, mollis erat facilisi posuere rutrum luctus quis imperdiet pretium.
-      </p>
+PEATmoss is an expression atlas for bryophyte gene expression data.
+             The <i>Physcomitrium patens</i> Gene Model Lookup Database (PpGML DB), contains tools,
+             sequences and annotations to lookup between the different gene model version of <i>P. patens</i>.      </p>
       <p>
-        Risus sollicitudin facilisis integer luctus hendrerit aliquet natoque senectus suspendisse, 
-        parturient facilisi laoreet tempor hac cum mi egestas ridiculus, accumsan lacinia inceptos diam commodo per bibendum nullam. 
-        Morbi blandit a suspendisse quis nascetur dignissim tellus, ante ad curabitur leo mauris taciti sollicitudin habitasse, 
-        diam pharetra euismod tristique aliquet nam. Dis nascetur vivamus justo libero platea interdum feugiat tellus, 
-        ad commodo nec etiam volutpat imperdiet viverra, habitasse suspendisse arcu porttitor lacinia iaculis aenean.
-      </p>
-      <img class='rounded' src='<?php echo "$images_path/header_img.png";?>' width="900px" alt='img example'>
-      
+
+
+<h4>Please, when using this web site or its data, cite us using the reference:</h4>
+
+    <div class="card bg-light">
+      <div class= "card-body" style="padding: 1rem;">
+        Fernandez-Pozo, N., Haas, F.B., Meyberg, R., Ullrich, K.K., Hiss, M., Perroud, P.F., Hanke, S., Kratz, V., Powell, A.F., Vesty, E.F., Daum, C.G., Zane, M., Lipzen, A., Sreedasyam, A., Grimwood, J., Coates, J.C., Barry, K., Schmutz, J., Mueller, L.A. and Rensing, S.A. (2020).
+        PEATmoss (Physcomitrella Expression Atlas Tool): a unified gene expression atlas for the model plant <i>Physcomitrella patens</i>.<br>
+        <a href="https://doi.org/10.1111/tpj.14607" target="_blank">Plant J., 102:165</a><br>
+      </div>
+    </div>
+
+
+
+<br>
+
