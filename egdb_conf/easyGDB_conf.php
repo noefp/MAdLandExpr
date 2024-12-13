d3a51b866ee3feb7793e08f8a8ad911b820f5610
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-12-13 17:09:41 +0100

    json files reorganized

--- egdb_conf/easyGDB_conf.php
+++ egdb_conf/easyGDB_conf.php
@@ -4,50 +4,65 @@ $root_path = "/var/www/html"; //use absolute path
 $egdb_files_folder = "MAdLandExpr";
 
 $easy_gdb_path = "$root_path/easy_gdb";
-$blast_dbs_path = "$root_path/blast_dbs";
-$expression_path = "$root_path/expression_data/peatmoss2";
-$downloads_path = "downloads";
-$lookup_path = "$root_path/lookup/madland"; //from root 
 $images_path = "/$egdb_files_folder/egdb_images";
 $custom_text_path = "$root_path/$egdb_files_folder/egdb_custom_text";
+$downloads_path = "downloads";
+$annotations_path = "$root_path/annotations";
+$blast_dbs_path = "$root_path/blast_dbs";
+$lookup_path = "$root_path/lookup/madland"; //from root 
 $species_path = "$root_path/$egdb_files_folder/egdb_species";
 $lab_path = "$root_path/$egdb_files_folder/egdb_labs";
-$annotations_path = "$root_path/annotations";
-$annotation_links_path = "$root_path/$egdb_files_folder/annotations";
+$json_files_path = "$root_path/$egdb_files_folder/json_files";
 
 // Custom css file
-//$custom_css = 1;
 $custom_css_path = "$egdb_files_folder/css/madland.css";
 
-// Select 1 to store annotations in files or 0 to store annotations in a relational database
-$file_database = 1;
+// $annotation_links_path = "$root_path/$egdb_files_folder/annotations";
 
-// header
+//Expression
+$expression_path = "$root_path/expression_data/peatmoss2";
+$private_expression_path = "$root_path/private_expression_data/peatmoss2";
+
+//Passport
+$passport_path = "$root_path/passport";
+$phenotype_imgs_path = "$images_path/descriptors_imgs";
+
+// header: site title, header image and logo
 $dbTitle = "MAdLandExpr";
 $header_img = "cover.jpg";
 $db_logo = "logos/madland_logo.jpg";
 
+// Select 1 to store annotations in files or 0 to store annotations in a relational database
+$file_database = 1;
+
 // Toolbar
+$tb_custom = 1;
 $tb_rm_home = 1;
+
 $tb_about = 0;
 $tb_downloads = 0;
 $tb_species = 0;
 $tb_search_box = 0;
+
 $tb_tools = 0;
 $tb_search = 0;
 $tb_blast = 0;
 $tb_jbrowse = 0;
 $tb_seq_ext = 0;
 $tb_annot_ext = 0;
-$tb_gene_expr = 0;
 $tb_lookup = 0;
+$tb_enrichment = 0;
 $tb_more = 0;
-$tb_custom = 1;
+
+//Gene expression atlas
+$tb_gene_expr = 0;
+
+// About
+$ab_citation = 0;
+$ab_labs = 0;
 
 // Expression Atlas
-//$expr_cards = 1;
 $expr_menu = 1;
-//$expr_cartoons = 0;
 $comparator_lookup = 1;
 
 // Expression tools order: 0 for not shown, >=1 to setup the order
@@ -61,10 +76,9 @@ $positions=[
   'table' => 7
 ];
 
-
-// About
-$ab_citation = 0;
-$ab_labs = 0;
+// Passport
+$show_map = 1;
+$show_qr = 1;
 
 //Gene examples
 $gene_sample = "";
@@ -79,6 +93,7 @@ $max_lookup_input = 10000;
 $max_extract_seq_input = 10000;
 $max_blast_input = 20;
 $max_expression_input = 100;
+$max_annotation_input = 15000;
 
 // BLAST
 $blast_example=">protein_or_DNA
