98331f5c3e84b68a372628730206bdb993a18ba6
Enrique López Gómez <enrique.elg.93@gmail.com>
2025-02-26 17:37:33 +0100

    Update welcome_text and add db_citation

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -36,15 +36,9 @@
   </div>
   <br>
 
-  <h4>Please, when using this web site or its data, cite us using the reference:</h4>
-
-    <div class="card bg-light">
-      <div class= "card-body" style="padding: 1rem;">
-        Fernandez-Pozo, N., Haas, F.B., Meyberg, R., Ullrich, K.K., Hiss, M., Perroud, P.F., Hanke, S., Kratz, V., Powell, A.F., Vesty, E.F., Daum, C.G., Zane, M., Lipzen, A., Sreedasyam, A., Grimwood, J., Coates, J.C., Barry, K., Schmutz, J., Mueller, L.A. and   Rensing, S.A. (2020).
-        PEATmoss (Physcomitrella Expression Atlas Tool): a unified gene expression atlas for the model plant <i>Physcomitrella patens</i> .<br>
-        <a href="https://doi.org/10.1111/tpj.14607" target="_blank">Plant J., 102:165</a><br>
-      </div>
-    </div>
+  <?php 
+    include_once realpath("$custom_text_path/db_citation.php");
+  ?>
 
   <br>
 </div>
