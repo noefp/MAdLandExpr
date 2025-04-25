50a69fe980626ef4909e9fe3b4328d8938274398
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2025-04-25 12:57:26 +0200

    description updated

--- egdb_custom_text/expr_datasets/pp_sex_rep.php
+++ egdb_custom_text/expr_datasets/pp_sex_rep.php
@@ -1,7 +1,7 @@
 <p>
   RNA-seq expression data of <i>P. patens</i> experiments conducted in sexual organs and first stages of the sporophyte development. All samples were normalized to RPKM.
   
-  The experiments of this dataset were published in <a href="https://doi.org/10.1007/s00497-021-00409-0">Genau et al., 2021</a> (Gametophore apices), <a href="https://doi.org/10.1111/nph.16486">Meyberg et al., 2020</a> (antheridia bundles), <a href="https://doi.org/10.3389/fpls.2020.00813">Haas et al., 2020</a> (Egg cell, unfertilized archegonia, F, F1/2, and ES1), <a href="https://doi.org/10.1093%2Fgbe%2Fevx098">Szövényi et al., 2020</a> (F1/E1/E2, ES/ES1, Green sporophyte), <a href="https://doi.org/10.1038/s41477-021-00958-2">Julca et al., 2021</a> (antheridia development, sperm cell packages, and mature archegonia).
+  The experiments of this dataset were published in <a href="https://doi.org/10.1007/s00497-021-00409-0">Genau et al., 2021</a> (Gametophore apices), <a href="https://doi.org/10.1111/nph.16486">Meyberg et al., 2020</a> (antheridia 21 DAI), <a href="https://doi.org/10.3389/fpls.2020.00813">Haas et al., 2020</a> (unfertilized archegonia egg cell and venter cells, E0 embryo adn venter cells, and ES1), <a href="https://doi.org/10.1093%2Fgbe%2Fevx098">Szövényi et al., 2020</a> (E0/E1/E2, ES/ES1, green sporophyte premeiotic), <a href="https://doi.org/10.1038/s41477-021-00958-2">Julca et al., 2021</a> (Reute antheridia development, sperm cell packages, and mature archegonia).
   
 </p>
 
@@ -9,10 +9,10 @@
 
 
 <ul>
-  <li><span style="color:blue;">Re - Gam apice noW (gametophore apices without watering)</span></li>
+  <li><span style="color:blue;">Re - Gam apice 21 DAI (gametophore apices without watering)</span></li>
   <li><span style="color:blue;">Re - Gam apice 2 DAW (gametophore apices 2 days after watering)</span></li>
   <hr>
-  <li><span style="color:green;">Re - Antheridia Bundles</span></li>
+  <li><span style="color:green;">Re - Antheridia 21 DAI</span></li>
   <hr>
   <li><span style="color:green;">Re - Antheridia 9 DAI (antheridia 9 days after induction)</span></li>
   <li><span style="color:green;">Re - Antheridia 11 DAI (antheridia 11 days after induction)</span> [5 replicates]</li>
@@ -20,15 +20,15 @@
   <li><span style="color:green;">Re - Sperm Cell Packages</span></li>
   <li><span style="color:orange;">Re - Mature Archegonia</span></li>
   <hr>
-  <li><span style="color:orange;">Vx - EggCell Mat Archegonia (egg cell mature archegonia)</span></li>
-  <li><span style="color:orange;">Vx - Unfertilized Archegonia</span></li>
-  <li><span style="color:pink;">Vx - F</span></li>
-  <li><span style="color:pink;">Vx - F1/2</span></li>
+  <li><span style="color:orange;">Vx - Unfertilized archegonia egg cell</span></li>
+  <li><span style="color:orange;">Vx - Unfertilized archegonia venter cells</span></li>
+  <li><span style="color:pink;">Vx - E0 Embryo</span></li>
+  <li><span style="color:pink;">Vx - E0 Venter cells</span></li>
   <li><span style="color:pink;">Re - ES1</span> [1 replicate]</li>
   <hr>
-  <li><span style="color:pink;">Vx - F1/E1/E2</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx - E0/E1/E2</span> [1 replicate]</li>
   <li><span style="color:pink;">Vx - ES/ES1</span> [1 replicate]</li>
-  <li><span style="color:pink;">Vx - Green Sporophyte</span> [1 replicate]</li>
+  <li><span style="color:pink;">Vx - Green Sporophyte PM</span> [1 replicate]</li>
   
   
 </ul>
@@ -36,9 +36,8 @@
 <p>
   Re: Reute WT<br>
   Vx: Villersexel WT<br>
-  noW: 2 days without watering<br>
-  2 DAW: 2 days after watering<br>
   DAI: days after induction<br>
+  2 DAW: 2 days after watering<br>
   Note: Gametophores apices were collected to study gene expression in sexual organs.
 </p>
 
@@ -48,56 +47,65 @@
 <p>Figure 1. Sporophyte developmental stages</p>
 </center>
 
-<table class="spodev_table">
-<tr style="border-bottom: solid 1px">
-    <td>Abb.</td>
-    <td style="width:210px"><b>Developmental stage</b></td>
-    <td><b>Description</b></td>
-  </tr>
-  <tr>
-    <td>E1</td>
-    <td><b>Embryo 1:</b><br></td>
-    <td>Developing embryo; the upper, chloroplast rich half will develop into  the spherical,
-    spore-containing spore capsule, whereas the lower part  will connect to the gametophore for 
-    nourishment of the developing  sporophyte.
-    </td>
-  </tr>
-  <tr>
-    <td>E2</td>
-    <td><b>Embryo 2:</b><br></td>
-    <td>Elongated embryo without developed stomata. Connection between
-gametophore and sporophyte still loose but cells started to differentiate.</td>
-  </tr>
-  <tr>
-    <td>ES</td>
-    <td><b>Early sporophyte:</b></td>
-    <td>Stomata are developed, clear separation of capsule  (inflation) and developing seta (2n).</td>
-  </tr>
-  <tr>
-    <td>PM</td>
-    <td><b>Premeiotic sporophyte:</b></td>
-    <td>Spherical green translucend sporophyte containing spore mother cells (2n), seta starts to turn brownish.</td>
-  </tr>
-  <tr>
-    <td>M</td>
-    <td><b>Meiotic sporophyte:</b></td>
-    <td>Opaque green sporophyte containing tetrades which will later form each four spores (1n, meiosis occured).</td>
-  </tr>
-  <tr>
-    <td>Y</td>
-    <td><b>Yellow sporophyte:</b></td>
-    <td>Contains early spores of the final size covered by a plasma membrane, seta darkend during the maturation process.</td>
-  </tr>
-  <tr>
-    <td>LB</td>
-    <td><b>Light brown sporophyte:</b></td>
-    <td>Spores started to mature, the spore wall thickens by exine deposition.</td>
-  </tr>
-  <tr style="border-bottom: solid 1px; padding-bottom:20px;height:45px">
-    <td>B</td>
-    <td><b>Brown sporophyte:</b></td>
-    <td>Spores are mature, spore wall consists of a thick perine layer with spikes, exine, seperating layer and intine, sporophyte detaches easily.</td>
-  </tr>
+<table class="spodev_table table-striped" style="width:100%">
+  <thead>
+    <tr style="border-bottom: solid 1px">
+      <th>Abb.</th>
+      <th>Developmental stage</th>
+      <th style="width:100%">Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>E0</td>
+      <td><b>Embryo 0:</b><br></td>
+      <td>Embryos of archegonia 2 days after watering/fertilization (8 cells aprox/stage E0 following Meyberg et al 2020 stage specification)</td>
+    </tr>
+    <tr>
+      <td>E1</td>
+      <td><b>Embryo 1:</b><br></td>
+      <td>Developing embryo; the upper, chloroplast rich half will develop into  the spherical,
+      spore-containing spore capsule, whereas the lower part  will connect to the gametophore for 
+      nourishment of the developing  sporophyte.
+      </td>
+    </tr>
+    <tr>
+      <td>E2</td>
+      <td><b>Embryo 2:</b><br></td>
+      <td>Elongated embryo without developed stomata. Connection between
+  gametophore and sporophyte still loose but cells started to differentiate.</td>
+    </tr>
+    <tr>
+      <td>ES</td>
+      <td><b>Early sporophyte:</b></td>
+      <td>Stomata are developed, clear separation of capsule  (inflation) and developing seta (2n).</td>
+    </tr>
+    <tr>
+      <td>PM</td>
+      <td><b>Premeiotic sporophyte:</b></td>
+      <td>Spherical green translucend sporophyte containing spore mother cells (2n), seta starts to turn brownish.</td>
+    </tr>
+    <tr>
+      <td>M</td>
+      <td><b>Meiotic sporophyte:</b></td>
+      <td>Opaque green sporophyte containing tetrades which will later form each four spores (1n, meiosis occured).</td>
+    </tr>
+    <tr>
+      <td>Y</td>
+      <td><b>Yellow sporophyte:</b></td>
+      <td>Contains early spores of the final size covered by a plasma membrane, seta darkend during the maturation process.</td>
+    </tr>
+    <tr>
+      <td>LB</td>
+      <td><b>Light brown sporophyte:</b></td>
+      <td>Spores started to mature, the spore wall thickens by exine deposition.</td>
+    </tr>
+    <tr style="border-bottom: solid 1px; padding-bottom:20px;height:45px">
+      <td>B</td>
+      <td><b>Brown sporophyte:</b></td>
+      <td>Spores are mature, spore wall consists of a thick perine layer with spikes, exine, seperating layer and intine, sporophyte detaches easily.</td>
+    </tr>
+  </tbody>
 </table>
 
 <br>
