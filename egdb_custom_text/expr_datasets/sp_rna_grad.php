0caf50cb5c05ad98d4cec9d4b3a07596ceec35a8
Enrique López Gómez <enrique.elg.93@gmail.com>
2025-05-08 18:40:19 +0200

    Update gradient dataset

--- egdb_custom_text/expr_datasets/sp_rna_grad.php
+++ egdb_custom_text/expr_datasets/sp_rna_grad.php
@@ -1,5 +1,5 @@
 <p>
-  Comprehensive dataset of <i>Spirogyra pratensis</i> gene expression along a gradient of 42 samples plus control samples, covering 43 samples in total.
+  Comprehensive dataset of <i>Spirogyra pratensis</i> gene expression across a gradient of 42 samples with different exposure to light intensity and temperature, plus control samples, covering 43 samples in total.
 </p>
 
 <h4>Experimental Conditions (3 replicates for each sample):</h4>
@@ -8,3 +8,28 @@
   <li><span style="color:green;">Samples</span> [42 samples]</li>
   <li><span style="color:blue;">Control</span> [1 sample]</li>
 </ul>
+
+<img src="/MAdLandExpr/egdb_images/expr/gradient_table_conditions.png" style="max-width: 350px;">
+<p><b>Figure 1.</b> Schematic representation of the enviromental conditions.</p>
+
+
+<table>
+  <thead>
+    <tr>
+      <th colspan="2" style="text-align: center; border-bottom: 1px solid #000; padding: 6px;">
+        <strong>Abbreviations</strong>
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td style="padding: 6px; text-align: left;"> <b>um</b> </td>
+      <td style="padding: 6px; text-align: left;"> &mu;mol photons s<sup>&minus;1</sup> m<sup>&minus;2</sup> </td>
+    </tr>
+    <tr>
+      <td style="padding: 6px; text-align: left;"> <b>C</b> </td>
+      <td style="padding: 6px; text-align: left;"> &deg;C </td>
+    </tr>
+  </tbody>
+</table>
+<br>
