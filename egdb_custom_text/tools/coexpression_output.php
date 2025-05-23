92ff548587d1f1e639e3beb4d2f0c35c33784535
Enrique López Gómez <enrique.elg.93@gmail.com>
2025-05-23 14:25:06 +0200

    Create coexpression output optional text

---
+++ egdb_custom_text/tools/coexpression_output.php
@@ -0,0 +1,6 @@
+<div class="alert alert-info" role="alert">
+  <p style="font-size: 17px; text-align: center; margin-bottom: 0;">
+Correlation coefficient (Pearson) between the query gene and other genes within the same WGCNA module.
+Only positive correlations ≥ 0.8 are shown.
+  </p>
+</div>
