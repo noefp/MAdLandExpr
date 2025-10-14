15484e6c03f4adc10bdabb11bfd767da0c4fd633
Noe Fernandez Pozo <noefernandezpozo@macbookpro.home>
2025-10-14 15:37:03 +0200

    Zygnema spp. added

---
+++ egdb_species/zygnema.php
@@ -0,0 +1,36 @@
+<br>
+<img class="float-right" style="z-index:0;height:150px" src="<?php echo $images_path.'/species/'.$sps_img ?>" >
+<h1><?php echo $sps_subtitle ?></h1>
+<h3 style="color:#666"><i><?php echo $sps_title ?></i></h3>
+<a href="https://phycocosm.jgi.doe.gov/phycocosm/home" class="btn btn-info sps-btn" role="button" target="_blank">PhycoCosm</a>
+<a href="https://phycocosm.jgi.doe.gov/Zygcyl6981a_1/Zygcyl6981a_1.home.html" class="btn btn-info sps-btn" role="button" target="_blank"><i>Z. cf. cylindricum</i> SAG 698-1a</a>
+<a href="https://phycocosm.jgi.doe.gov/Zygcir6981b_2/Zygcir6981b_2.home.html" class="btn btn-info sps-btn" role="button" target="_blank"><i>Z. cf. cylindricum</i> SAG 698-1b</a>
+<a href="https://phycocosm.jgi.doe.gov/Zygcir1559_1/Zygcir1559_1.home.html" class="btn btn-info sps-btn" role="button" target="_blank"><i>Z. circumcarinatum</i> UTEX 1559</a>
+<a href="https://phycocosm.jgi.doe.gov/Zygcir1560_1/Zygcir1560_1.home.html" class="btn btn-info sps-btn" role="button" target="_blank"><i>Z. circumcarinatum</i> UTEX 1560</a>
+<br style="clear:both"/>
+<br>
+
+<p>
+Zygnematophyceae are the algal sisters of land plants.
+This class is named for filamentous algae of the genus Zygnema, which are known to occur in challenging environments and are desiccation resistant. 
+Four strains (SAG 698-1a, SAG 698-1b, UTEX 1559, UTEX 1560) were sequenced, capturing the strain history of the + and - mating types derived from the Zygnema circumcarinatum sample of 1929 by Czurda. 
+Of these, SAG 698-1a likely now represents a different species, Zygnema cf. cylindricum. 
+Owing to their key phylogenetic position, genome data on these organisms shed light on the evolution of the genetic toolbox for multicellular growth and response to environmental challenges.
+</p>
+<br>
+
+<div class="row">
+  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+    <h4>Please, when using this web site or its data, cite us using the reference:</h4>
+    <div class="card bg-light">
+      <div class= "card-body" style="padding: 1rem;">
+        Feng X, Zheng J, Irisarri I, Yu H, Zheng B, Ali Z, de Vries S, Keller J, Fürst-Jansen JMR, Dadras A, Zegers JMS, Rieseberg TP, Dhabalia Ashok A, Darienko T, Bierenbroodspot MJ, Gramzow L, Petroll R, Haas FB, Fernandez-Pozo N, Nousias O, Li T, Fitzek E, Grayburn WS, Rittmeier N, Permann C, Rümpler F, Archibald JM, Theißen G, Mower JP, Lorenz M, Buschmann H, von Schwartzenberg K, Boston L, Hayes RD, Daum C, Barry K, Grigoriev IV, Wang X, Li FW, Rensing SA, Ben Ari J, Keren N, Mosquna A, Holzinger A, Delaux PM, Zhang C, Huang J, Mutwil M, de Vries J, Yin Y
+        Genomes of multicellular algal sisters to land plants illuminate signaling network evolution.<br>
+        <a href="https://doi.org/10.1038/s41588-024-01737-3" target="_blank">Nat Genet. 2024 May;56(5):1018-1031. doi: 10.1038/s41588-024-01737-3</a><br>
+      </div>
+    </div>
+
+  </div>
+</div>
+
